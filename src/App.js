560a43d4dd6ec7829dd2ff896a8b1d7090076343
Ahmed <ahmeedsayed01@gmail.com>
2023-09-27 04:20:25 +0300

    added search component to the home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
-
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/components/Search/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHand } from "@fortawesome/free-solid-svg-icons";
+import "./search.css";
+export default function Search() {
+  return (
+    <div className=" search p-4 text-start my-3 mx-4 rounded shadow">
+      <h2 className="text-start">
+        <FontAwesomeIcon
+          icon={faHand}
+          size="xlg"
+          style={{ color: "#ffffff" }}
+        />{" "}
+        Welcome to our movie app
+      </h2>
+      <div className="container my-3 mx-4">
+        <h6>
+          Millions of movies, TV shows and people to discover. Explore now.
+        </h6>
+
+        <div className="row align-items-center mt-4">
+          <div className="col col-10">
+            <input
+              type="text"
+              className="form-control shadow"
+              placeholder="Search and Explore"
+            />
+          </div>
+          <div className="col">
+            <button className="btn btn-warning px-4 py-2 shadow">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
