e57f904cb564bbf2cd7311f4d743f84474b4b6af
Ahmed-Mahdi0 <ahmeedsayed01@gmail.com>
2023-10-03 00:53:57 +0300

    added watchlist counter

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const favoriteArray = useSelector((state) => state.favoriteArray);
   return (
     <>
       <nav className="navbar sticky-top  nav-dark bg-warning  ">
@@ -12,11 +14,17 @@ export default function Header() {
             MOVIE APP
           </Link>
           <Link
-            className="d-flex  align-items-center text-decoration-none text-dark me-4"
-            to={'/watchlist'}
+            className="d-flex  align-items-center text-decoration-none text-dark me-2 "
+            to={"/watchlist"}
           >
             <FontAwesomeIcon className="me-2" icon={faHeart} size="lg" />
-            <div className="fw-bold">WatchList</div>
+            <div className="fw-bold position-relative p-1">
+              {" "}
+              WatchList
+              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-light text-dark">
+                {favoriteArray.length}
+              </span>{" "}
+            </div>
           </Link>
         </div>
       </nav>
