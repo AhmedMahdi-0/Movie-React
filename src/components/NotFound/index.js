c638ce7e266118e8a3af23bb0c98c18cd4e29b3e
Ayman97 <engaymanfarag1997@gmail.com>
2023-10-02 09:38:47 +0300

    adjusted watchlist page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function NotFound() {
   return (
-    <div class> <h1 className='text-center p-5'>Page Not Found</h1></div>
-  )
+    <div class>
+      {" "}
+      <h1 className="text-center p-5 mt-5">Page Not Found</h1>
+    </div>
+  );
 }
